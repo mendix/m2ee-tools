1defe45a49f7248ec04adb832cf1b912df441401
Achiel van der Mandele <achiel.van.der.mandele@mendix.com>
2012-09-14 12:25:22 +0200

    refactored nagios check into separate methods

--- src/m2ee/nagios.py
+++ src/m2ee/nagios.py
@@ -11,16 +11,31 @@ STATE_CRITICAL=2
 STATE_UNKNOWN=3
 STATE_DEPENDENT=4
 
+RUNNING=-1
+HEALTHY=-2
+
 def check(runner, client):
 
+    runtime_state = check_process(runner, client)
+    if runtime_state != RUNNING:
+        return runtime_state
+
+    runtime_health = check_health(client)
+
+    if runtime_health != HEALTHY:
+        return runtime_health
+
+    # everything seems to be fine, print version info and exit
+    about_feedback = client.about().get_feedback()
+    print "MxRuntime OK: healthy, using version %s" % about_feedback['version']
+    return STATE_OK
+
+def check_process(runner, client):
     pid = runner.get_pid()
 
-    # are we supposed to be running?
     if pid == None:
         print "MxRuntime OK: Not running."
         return STATE_OK
-
-    #
     pid_alive = runner.check_pid()
     m2ee_alive = client.ping()
 
@@ -32,7 +47,6 @@ def check(runner, client):
         print "MxRuntime CRITICAL: pid %s is not available, m2ee does not respond." % pid
         return STATE_CRITICAL
 
-    # look if any critical message was logged
     errors = client.get_critical_log_messages()
     if len(errors) != 0:
         print "MxRuntime CRITICAL: %d critical error(s) were logged" % len(errors)
@@ -48,7 +62,6 @@ def check(runner, client):
         print "MxRuntime WARNING: plugin has broken logic, m2ee should be alive"
         return STATE_WARNING
 
-    # check status, running is OK, starting is WARNING, everything else is CRITICAL
     status_feedback = client.runtime_status().get_feedback()
     if status_feedback['status'] == 'starting':
         print "MxRuntime WARNING: application is still starting up..."
@@ -57,9 +70,9 @@ def check(runner, client):
         print "MxRuntime CRITICAL: application is in state %s" % status_feedback['status']
         return STATE_CRITICAL
 
-    # so runtime state is 'running' if we've arrived here
+    return RUNNING
 
-    # let's do a health check
+def check_health(client):
     health_response = client.check_health()
     if not health_response.has_error():
         feedback = health_response.get_feedback()
@@ -86,9 +99,5 @@ def check(runner, client):
         else:
             print "MxRuntime WARNING: Health check failed unexpectedly: %s" % health_response.get_error()
             return STATE_WARNING
-
-    # everything seems to be fine, print version info and exit
-    about_feedback = client.about().get_feedback()
-    print "MxRuntime OK: healthy, using version %s" % about_feedback['version']
-    return STATE_OK
+    return HEALTHY
 
