fccdae7ac48ae5651ceccef1be7f62f3c19123a9
Hans van Kranenburg <hans.van.kranenburg@mendix.com>
2012-10-10 00:23:34 +0200

    move some advanced commands to default help
    show_current_runtime_requests, interrupt_request, and
    show_license_information are now in the default help output

--- src/m2ee.py
+++ src/m2ee.py
@@ -680,6 +680,9 @@ class CLI(cmd.Cmd):
         print " log - follow live logging from the application"
         print " loglevel - view and configure loglevels"
         print " about - show Mendix Runtime version information"
+        print " show_current_runtime_requests - show action stack of currently handled requests"
+        print " interrupt_request - cancel a running runtime request"
+        print " show_license_information - show details about current mendix license key"
         print " exit, quit, <ctrl>-d - exit m2ee"
         print
 
@@ -694,12 +697,9 @@ class CLI(cmd.Cmd):
         if args == 'expert':
             print "Advanced commands:"
             print " statistics - show all application statistics that can be used for monitoring"
-            print " show_current_runtime_requests - show action stack of currently handled requests"
-            print " interrupt_request - cancel a running runtime request"
             print " show_all_thread_stack_traces - show all low-level JVM threads with stack trace"
             print " profiler - start the profiler (experimental) "
             print " check_health - manually execute health check"
-            print " show_license_information - show details about current mendix license key"
             print
             print "Extra commands you probably don't need:"
             print " debug - dive into a local python debug session inside this program"
