mendix/m2ee-tools

commit 82623bb46ba45664727930551c21c22e521148b7

Add more debug/trace logging to JVM startup and PostgreSQL actions

src/m2ee/client.py
@@ -45,7 +45,8 @@ class M2EEClient:
         except AttributeError:
             # httplib throws AttributeError: 'NoneType' object has no attribute 'makefile' :-|
             pass
-        except SocketError:
+        except SocketError, e:
+            logger.trace("Got SocketError: %d (%s)" % (e.errno, e.strerror))
             pass
         return False
 

src/m2ee/config.py
@@ -348,13 +348,15 @@ class M2EEConfig:
             host = host[:lastcolon]
 
         # TODO: sanity checks
-        return {
+        pg_env = {
             'PGHOST': host,
             'PGPORT': port,
             'PGUSER': self._conf['mxruntime']['DatabaseUserName'],
             'PGPASSWORD': self._conf['mxruntime']['DatabasePassword'],
             'PGDATABASE': self._conf['mxruntime']['DatabaseName'],
         }
+        logger.trace("PostgreSQL environment variables: %s" % str(pg_env))
+        return pg_env
 
     def get_psql_location(self):
         return self._conf['mxnode'].get('psql', 'psql')

src/m2ee/pgutil.py
@@ -26,6 +26,7 @@ class M2EEPgUtil:
 
         logger.info("Writing database dump to %s" % db_dump_file_name)
         cmd = (pg_dump_location, "-O", "-x", "-F", "c")
+        logger.trace("Executing %s" % str(cmd))
         proc = subprocess.Popen(cmd, env=pgenv, stdout=open(db_dump_file_name, 'w+'))
         proc.communicate()
 
@@ -50,6 +51,7 @@ class M2EEPgUtil:
 
         logger.debug("Restoring %s" % db_dump_file_name)
         cmd = (pg_restore_location, "-d", pgenv['PGDATABASE'], "-O", "-x", db_dump_file_name)
+        logger.trace("Executing %s" % str(cmd))
         proc = subprocess.Popen(cmd, env=pgenv, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (stdout,stderr) = proc.communicate()
 
@@ -86,6 +88,7 @@ class M2EEPgUtil:
             "c.relnamespace WHERE relkind = 'r' AND n.nspname NOT IN ('pg_catalog', "
             "'pg_toast') AND pg_catalog.pg_table_is_visible(c.oid)"
         )
+        logger.trace("Executing %s, creating pipe for stdout,stderr" % str(cmd))
         proc1 = subprocess.Popen(cmd, env=pgenv, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (stdout,stderr) =  proc1.communicate()
 
@@ -95,6 +98,7 @@ class M2EEPgUtil:
         
         stdin = stdout
         cmd = (psql_location,)
+        logger.trace("Piping stdout,stderr to %s" % str(cmd))
         proc2 = subprocess.Popen(cmd, env=pgenv, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (stdout,stderr) = proc2.communicate(stdin)
         
@@ -110,6 +114,7 @@ class M2EEPgUtil:
             "c.relnamespace WHERE relkind = 'S' AND n.nspname NOT IN ('pg_catalog', "
             "'pg_toast') AND pg_catalog.pg_table_is_visible(c.oid)"
         )
+        logger.trace("Executing %s, creating pipe for stdout,stderr" % str(cmd))
         proc1 = subprocess.Popen(cmd, env=pgenv, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (stdout,stderr) =  proc1.communicate()
 
@@ -119,6 +124,7 @@ class M2EEPgUtil:
         
         stdin = stdout
         cmd = (psql_location,)
+        logger.trace("Piping stdout,stderr to %s" % str(cmd))
         proc2 = subprocess.Popen(cmd, env=pgenv, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (stdout,stderr) = proc2.communicate(stdin)
         
@@ -130,4 +136,6 @@ class M2EEPgUtil:
         pgenv = os.environ.copy()
         pgenv.update(self._config.get_pg_environment())
         psql_location = self._config.get_psql_location()
-        subprocess.call((psql_location,), env=pgenv)
+        cmd = (psql_location,)
+        logger.trace("Executing %s" % str(cmd))
+        subprocess.call(cmd, env=pgenv)

src/m2ee/runner.py
@@ -61,7 +61,7 @@ class M2EERunner:
             os.kill(pid, 0)
             return True
         except OSError:
-            logger.debug("No process with pid %s, or not ours." % pid)
+            logger.trace("No process with pid %s, or not ours." % pid)
             return False
 
     def stop(self, timeout=5):
@@ -98,10 +98,21 @@ class M2EERunner:
             logger.error("Cannot start MxRuntime, MxRuntime version %s is not available on this server" % version)
             return False
 
+        # prepare execution
+        cmd = ['java']
+        javaopts = self._config.get_java_opts()
+        if javaopts:
+            cmd.extend(javaopts)
+        cmd.extend(['-cp', classpath, self._config.get_appcontainer_mainclass()])
+        env = self._config.get_java_env()
+        logger.trace("Command line to be used when starting the JVM: %s" % cmd)
+
         try:
+            logger.trace("[%s] Forking now..." % os.getpid()) 
             pid = os.fork()
             if pid > 0:
                 self._pid = None
+                logger.trace("[%s] Waiting for intermediate process to exit..." % os.getpid()) 
                 # prevent zombie process
                 (waitpid, result) = os.waitpid(pid, 0)
                 if result == 0:
@@ -112,26 +123,23 @@ class M2EERunner:
         except OSError, e:
             logger.error("Forking subprocess failed: %d (%s)\n" % (e.errno, e.strerror))
             return
+        logger.trace("[%s] Now in intermediate forked process..." % os.getpid())
         # decouple from parent environment
         os.chdir("/")
         os.setsid()
         os.umask(0022)
         # start java subprocess (second fork)
-        cmd = ['java']
-        javaopts = self._config.get_java_opts()
-        if javaopts:
-            cmd.extend(javaopts)
-        cmd.extend(['-cp', classpath, self._config.get_appcontainer_mainclass()])
-        logger.trace("Starting java using command line: %s" % cmd)
+        logger.trace("[%s] Starting the JVM..." % os.getpid())
         proc = subprocess.Popen(
             cmd,
             close_fds=True,
             cwd='/',
-            env=self._config.get_java_env()
+            env=env,
         )
         # always write pid asap, so that monitoring can detect apps that should
         # be started but fail to do so
         self._pid = proc.pid
+        logger.trace("[%s] Writing JVM pid to pidfile: %s" % (os.getpid(),self._pid))
         self._write_pidfile()
         # wait for m2ee to become available
         t = 0
@@ -140,16 +148,22 @@ class M2EERunner:
             dead = proc.poll()
             if dead != None:
                 logger.error("Java subprocess terminated with errorcode %s" % dead)
+                logger.debug("[%s] Doing unclean exit from intermediate process now." % os.getpid())
                 os._exit(1)
             if self.check_pid(proc.pid) and self._client.ping():
                 break
             t += step
         if t >= timeout:
+            logger.error("Timeout: Java subprocess takes too long to start.")
+            logger.trace("[%s] Doing unclean exit from intermediate process now." % os.getpid())
             os._exit(1)
+        logger.trace("Calling CloseStdIO...") 
         self._client.close_stdio().display_error()
+        logger.trace("[%s] Exiting intermediate process..." % os.getpid()) 
         os._exit(0)
 
     def _wait_pid(self, timeout=None, step=0.25):
+        logger.trace("Waiting for process to disappear: timeout=%s" % timeout)
         if self.check_pid():
             if timeout == None:
                 return False
@@ -160,6 +174,7 @@ class M2EERunner:
                     break
                 t += step
             if t >= timeout:
+                logger.trace("Timeout: Process %s takes too long to disappear." % self._pid)
                 return False
         self._cleanup_pid()
         return True

commit f9d4e7a88fabcd03e4c8f732a305a398a999585b

Simplify exception handling on unusable m2ee api.
Let exception and error objects describe themselves... e.g.:
TRACE: Got <class 'socket.timeout'>: timed out
TRACE: Got <class 'socket.error'>: [Errno 111] Connection refused
ERROR: Got <class 'httplib.InvalidURL'>: nonnumeric port: 'henk'

Hide socket.error and socket.timeout, it means communication with the
m2ee api is broken. Print other exceptions with stacktrace, but
continue.

A Timeout can occur when the JVM is broken after e.g. a heap space
error. Normally, connection refused is the sign that the application is
just not running.

This fixes a 'bug' introduced in
82623bb46ba45664727930551c21c22e521148b7 where the trace logging was
added. This code assumes errno is filled with a number, but this is only
in case it is a socket.error. Well, socket.timeout also seems to be a
socket.error, but it does not have errno and strerror set. In this case
the string formatting that was added crashed on %d. This was already
quick-fixed in 138e04579626f6ba281350748f1aa52bce609cd3

src/m2ee/client.py
@@ -9,7 +9,6 @@ import httplib2
 import simplejson
 from base64 import b64encode
 import socket
-from socket import error as SocketError # python 2.5
 from log import logger
 
 class M2EEClient:
@@ -42,12 +41,12 @@ class M2EEClient:
             response = self.request("echo", {"echo":"ping"}, timeout)
             if response.get_result() == 0:
                 return True
-        except AttributeError:
-            # httplib throws AttributeError: 'NoneType' object has no attribute 'makefile' :-|
-            pass
-        except SocketError, e:
-            logger.trace("Got SocketError: %s (%s)" % (e.errno, e.strerror))
-            pass
+        except (socket.error, socket.timeout), e:
+            logger.trace("Got %s: %s" % (type(e), e))
+        except Exception, e:
+            logger.error("Got %s: %s" % (type(e), e))
+            import traceback
+            logger.error(traceback.format_exc())
         return False
 
     def echo(self, params=None):

src/m2ee/core.py
@@ -32,9 +32,13 @@ class M2EE():
         m2ee_alive = self.client.ping()
 
         if pid_alive and not m2ee_alive:
-            logger.error("The application process seems to be running (pid %s is alive), but does not respond to administrative requests." % self.runner.get_pid())
-            logger.error("This could be caused by JVM Heap Space / Out of memory errors. Please review the application logfiles.")
-            logger.error("You should consider restarting the application process, because it is likely to be in an undetermined broken state right now.")
+            logger.error("The application process seems to be running (pid %s is alive)," \
+                    " but is not accessible for administrative requests." % self.runner.get_pid())
+            logger.error("If this is not caused by a configuration error (e.g. wrong" \
+                    " admin_port) setting, it could be caused by JVM Heap Space / Out" \
+                    " of memory errors. Please review the application logfiles.")
+            logger.error("In case of JVM errors, you should consider restarting the application" \
+                    " process, because it is likely to be in an undetermined broken state right now.")
         elif not pid_alive and m2ee_alive:
             logger.error("pid %s is not available, but m2ee responds" % self.runner.get_pid())
         return (pid_alive, m2ee_alive)