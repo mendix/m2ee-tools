41337504dc96a05f0e66e98d96c409ed73848916
Jouke Thiemo Waleson <jouke@waleson.com>
2013-05-25 11:28:38 +0200

    Bug in unpacking with fix_mxclient_symlink when runtime not present

--- src/m2ee/mdautil.py
+++ src/m2ee/mdautil.py
@@ -84,7 +84,10 @@ def fix_mxclientsystem_symlink(config):
     mxclient_symlink = os.path.join(
         config.get_public_webroot_path(), 'mxclientsystem')
     real_mxclient_location = config.get_real_mxclientsystem_path()
-    if os.path.islink(mxclient_symlink):
+    if real_mxclient_location is None:
+        logger.error("MxClient location not found, can not set symlink. "
+                     "Please make sure that the runtime is installed.")
+    elif os.path.islink(mxclient_symlink):
         current_real_mxclient_location = os.path.realpath(
             mxclient_symlink)
         if current_real_mxclient_location != real_mxclient_location:
