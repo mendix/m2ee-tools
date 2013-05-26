mendix/m2ee-tools

commit bcead2e43387c4d1c239053b00cf687a5169eacb

Apify unpack
Also, move mxclientsystem symlink handling to unpack.
Move complete_unpack handling to the cli part of m2ee.
Move user input to cli.
Stop on unzip errors.

src/m2ee/core.py
@@ -13,6 +13,8 @@ from client import M2EEClient
 from runner import M2EERunner
 from log import logger
 
+import mdautil
+
 
 class M2EE():
 
@@ -113,33 +115,6 @@ class M2EE():
         else:
             self.runner.cleanup_pid()
         return True
-
-    def fix_mxclientsystem_symlink(self):
-        # check mxclientsystem symlink and refresh if necessary
-        if self.config.get_symlink_mxclientsystem():
-            mxclient_symlink = os.path.join(
-                self.config.get_public_webroot_path(), 'mxclientsystem')
-            real_mxclient_location = self.config.get_real_mxclientsystem_path()
-            if os.path.islink(mxclient_symlink):
-                current_real_mxclient_location = os.path.realpath(
-                    mxclient_symlink)
-                if current_real_mxclient_location != real_mxclient_location:
-                    logger.debug("mxclientsystem symlink exists, but points "
-                                 "to %s" % current_real_mxclient_location)
-                    logger.debug("redirecting symlink to %s" %
-                                 real_mxclient_location)
-                    os.unlink(mxclient_symlink)
-                    os.symlink(real_mxclient_location, mxclient_symlink)
-            elif not os.path.exists(mxclient_symlink):
-                logger.debug("creating mxclientsystem symlink pointing to %s" %
-                             real_mxclient_location)
-                try:
-                    os.symlink(real_mxclient_location, mxclient_symlink)
-                except OSError, e:
-                    logger.error("creating symlink failed: %s" % e)
-            else:
-                logger.warn("Not touching mxclientsystem symlink: file exists "
-                            "and is not a symlink")
 
     def _configure_logging(self):
         # try configure logging
@@ -245,3 +220,16 @@ class M2EE():
         fd = codecs.open(query_file_name, mode='w', encoding='utf-8')
         fd.write("%s" % '\n'.join(ddl_commands))
         fd.close()
+
+    def unpack(self, mda_name):
+        if mdautil.unpack(self.config, mda_name):
+            self.reload_config()
+        else:
+            return False
+
+        post_unpack_hook = self.config.get_post_unpack_hook()
+        if post_unpack_hook:
+            mdautil.run_post_unpack_hook(post_unpack_hook)
+
+        if self.config.get_symlink_mxclientsystem():
+            mdautil.fix_mxclientsystem_symlink(self.config)

src/m2ee.py
@@ -20,7 +20,7 @@ import pprint
 import yaml
 import datetime
 
-from m2ee import pgutil, M2EE, M2EEProfiler, mdautil
+from m2ee import pgutil, M2EE, M2EEProfiler
 from m2ee.log import logger
 from m2ee.config import find_yaml_files
 
@@ -110,10 +110,6 @@ class CLI(cmd.Cmd):
         """
         This function deals with the start-up sequence of the Mendix Runtime.
 
-        After a fixup of the mxclientsystem symlink, which needs to point to
-        the right version of the mxclientsystem code, and submitting runtime
-        configuration settings, the start action is called.
-
         Starting the Mendix Runtime can fail in both a temporary or permanent
         way.
 
@@ -133,8 +129,6 @@ class CLI(cmd.Cmd):
         Mendix Runtime is printed. Temporary failures need to be resolved,
         often interactively.
         """
-        self.m2ee.fix_mxclientsystem_symlink()
-
         if not self.m2ee.send_runtime_config():
             return
 
@@ -698,36 +692,26 @@ class CLI(cmd.Cmd):
             logger.error("The application process is still running, refusing "
                          "to unpack a new application model right now.")
             return
-        if mdautil.unpack(
-            self.m2ee.config.get_model_upload_path(),
-            args,
-            self.m2ee.config.get_app_base(),
-        ):
-            self.m2ee.reload_config()
-        post_unpack_hook = self.m2ee.config.get_post_unpack_hook()
-        if post_unpack_hook:
-            if os.path.isfile(post_unpack_hook):
-                if os.access(post_unpack_hook, os.X_OK):
-                    logger.info("Running post-unpack-hook...")
-                    retcode = subprocess.call((post_unpack_hook,))
-                    if retcode != 0:
-                        logger.error("The post-unpack-hook returned a "
-                                     "non-zero exit code: %d" % retcode)
-                else:
-                    logger.error("post-unpack-hook script %s is not "
-                                 "executable." % post_unpack_hook)
-            else:
-                logger.error("post-unpack-hook script %s does not exist." %
-                             post_unpack_hook)
+        logger.info("This command will replace the contents of the model/ and "
+                    "web/ locations, using the files extracted from the "
+                    "archive")
+        answer = raw_input("Continue? (y)es, (N)o? ")
+        if answer != 'y':
+            logger.info("Aborting!")
+            return
+        self.m2ee.unpack(args)
 
     def complete_unpack(self, text, line, begidx, endidx):
         # these complete functions seem to eat exceptions, which is very bad
         # behaviour if anything here throws an excaption, you just won't get
         # completion, without knowing why
-        return mdautil.complete_unpack(
-            self.m2ee.config.get_model_upload_path(),
-            text
-        )
+        model_upload_path = self.m2ee.config.get_model_upload_path()
+        logger.trace("complete_unpack: Looking for %s in %s" %
+                     (text, model_upload_path))
+        return [f for f in os.listdir(model_upload_path)
+                if os.path.isfile(os.path.join(model_upload_path, f))
+                and f.startswith(text)
+                and (f.endswith(".zip") or f.endswith(".mda"))]
 
     def do_log(self, args):
         if self._cleanup_logging():

src/m2ee/mdautil.py
@@ -17,15 +17,16 @@ try:
 except ImportError:
     pass
 
-def unpack(model_upload_path, mda_name, app_base):
+def unpack(config, mda_name):
 
-    mda_file_name = os.path.join(model_upload_path, mda_name)
+    mda_file_name = os.path.join(config.get_model_upload_path(), mda_name)
     if not os.path.isfile(mda_file_name):
         logger.error("file %s does not exist" % mda_file_name)
         return False
 
     logger.debug("Testing archive...")
     cmd = ("unzip", "-tqq", mda_file_name)
+    logger.trace("Executing %s" % str(cmd))
     try:
         proc = subprocess.Popen(cmd,
                                 stdout=subprocess.PIPE,
@@ -44,21 +45,16 @@ def unpack(model_upload_path, mda_name, app_base):
         logger.error("An error occured while executing unzip: %s" % ose)
         return False
 
-    logger.info("This command will replace the contents of the model/ and "
-                "web/ locations, using the files extracted from the archive")
-    answer = raw_input("Continue? (y)es, (N)o? ")
-    if answer != 'y':
-        logger.info("Aborting!")
-        return False
-
     logger.debug("Removing everything in model/ and web/ locations...")
     # TODO: error handling. removing model/ and web/ itself should not be
     # possible (parent dir is root owned), all errors ignored for now
+    app_base = config.get_app_base()
     shutil.rmtree(os.path.join(app_base, 'model'), ignore_errors=True)
     shutil.rmtree(os.path.join(app_base, 'web'), ignore_errors=True)
 
     logger.debug("Extracting archive...")
     cmd = ("unzip", "-oq", mda_file_name, "web/*", "model/*", "-d", app_base)
+    logger.trace("Executing %s" % str(cmd))
     proc = subprocess.Popen(cmd,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
@@ -77,10 +73,43 @@ def unpack(model_upload_path, mda_name, app_base):
     return True
 
 
-def complete_unpack(model_upload_path, text):
-    logger.trace("complete_unpack: Looking for %s in %s" %
-                 (text, model_upload_path))
-    return [f for f in os.listdir(model_upload_path)
-            if os.path.isfile(os.path.join(model_upload_path, f))
-            and f.startswith(text)
-            and (f.endswith(".zip") or f.endswith(".mda"))]
+def fix_mxclientsystem_symlink(config):
+    mxclient_symlink = os.path.join(
+        config.get_public_webroot_path(), 'mxclientsystem')
+    real_mxclient_location = config.get_real_mxclientsystem_path()
+    if os.path.islink(mxclient_symlink):
+        current_real_mxclient_location = os.path.realpath(
+            mxclient_symlink)
+        if current_real_mxclient_location != real_mxclient_location:
+            logger.debug("mxclientsystem symlink exists, but points "
+                         "to %s" % current_real_mxclient_location)
+            logger.debug("redirecting symlink to %s" %
+                         real_mxclient_location)
+            os.unlink(mxclient_symlink)
+            os.symlink(real_mxclient_location, mxclient_symlink)
+    elif not os.path.exists(mxclient_symlink):
+        logger.debug("creating mxclientsystem symlink pointing to %s" %
+                     real_mxclient_location)
+        try:
+            os.symlink(real_mxclient_location, mxclient_symlink)
+        except OSError, e:
+            logger.error("creating symlink failed: %s" % e)
+    else:
+        logger.warn("Not touching mxclientsystem symlink: file exists "
+                    "and is not a symlink")
+
+
+def run_post_unpack_hook(post_unpack_hook):
+    if os.path.isfile(post_unpack_hook):
+        if os.access(post_unpack_hook, os.X_OK):
+            logger.info("Running post-unpack-hook...")
+            retcode = subprocess.call((post_unpack_hook,))
+            if retcode != 0:
+                logger.error("The post-unpack-hook returned a "
+                             "non-zero exit code: %d" % retcode)
+        else:
+            logger.error("post-unpack-hook script %s is not "
+                         "executable." % post_unpack_hook)
+    else:
+        logger.error("post-unpack-hook script %s does not exist." %
+                     post_unpack_hook)

commit 07f2fd0eefc06d25ade19e3fca87a8c046db4db7

Move mxclientsystem symlink handling back to start
This fixes a bug introduced in bcead2e43387c4d1c239053b00cf687a5169eacb.

Moving fix_symlink from core to unpack was a bad idea, because it
fails really hard when unpacking an mda, while the associated runtime
is not present.

Now, the symlink will be set when starting the application, like it was
before.

src/m2ee/core.py
@@ -70,6 +70,9 @@ class M2EE():
                          "errors.")
             return False
 
+        if self.config.get_symlink_mxclientsystem():
+            mdautil.fix_mxclientsystem_symlink(self.config)
+
         logger.debug("Checking if the runtime is already alive...")
         (pid_alive, m2ee_alive) = self.check_alive()
         if not pid_alive and not m2ee_alive:
@@ -300,9 +303,6 @@ class M2EE():
         post_unpack_hook = self.config.get_post_unpack_hook()
         if post_unpack_hook:
             mdautil.run_post_unpack_hook(post_unpack_hook)
-
-        if self.config.get_symlink_mxclientsystem():
-            mdautil.fix_mxclientsystem_symlink(self.config)
 
     def _connect_xmpp(self):
         xmpp_credentials = self.config.get_xmpp_credentials()