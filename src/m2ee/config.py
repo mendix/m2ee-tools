mendix/m2ee-tools

commit 682259dbefe80bad5f23555b140a95c3e2f829d0

Mendix 5 runtime startup compatibility
Changeset #43885 in runtime broke startup procedure: create_runtime now
replaced by update_appcontainer_configuration. This doesn't actually
create or start a runtime, instead giving us a clear point on where we
can set jetty & other options for runtime.

Also, the location and name of the 'use_blocking_connector' option has
changed. It's now located within the 'jetty_options' jsonobject
and is called 'runtime_blocking_connector' instead.

Backwards compatibility for this is included, so people upgrading from
4.x to 5.x won't have to reconfigure the blocking connector setting.

src/m2ee/config.py
@@ -603,7 +603,13 @@ class M2EEConfig:
         return self._conf['logging']
 
     def get_jetty_options(self):
-        return self._conf['m2ee'].get('jetty', None)
+        result = copy.deepcopy(self._conf['m2ee'].get('jetty', {}))
+        already_specified = 'use_blocking_connector' in result
+        if self.get_runtime_version() >= 5 and not already_specified:
+            result['use_blocking_connector'] = \
+            self.get_runtime_blocking_connector()
+
+        return result
 
     def get_munin_options(self):
         return self._conf['m2ee'].get('munin', None)

src/m2ee/core.py
@@ -122,9 +122,7 @@ class M2EE():
                 "runtime_port": self.config.get_runtime_port(),
                 "runtime_listen_addresses":
                 self.config.get_runtime_listen_addresses(),
-                "use_blocking_connector":
-                self.config.get_runtime_blocking_connector(),
-                "jetty_options": self.config.get_jetty_options(),
+                "runtime_jetty_options": self.config.get_jetty_options()
             })
             response.display_error()
             self._connect_xmpp()

commit 2217e0b3cd38295a0a9b164b3dec9b8f39d093d5

Fix dictionary lookup on NoneType in jetty options
When the jetty: section is mentioned, but does not contain active
configuration items self._conf['m2ee'].get('jetty', {}) returns None.
The in operator doesn't like to operate on a NoneType.

    jetty:
     # comment
     #key: value

This bug was introduced in commit 682259dbefe80bad5f23555b140a95c3e2f829d0

src/m2ee/config.py
@@ -591,13 +591,14 @@ class M2EEConfig:
         return self._conf['logging']
 
     def get_jetty_options(self):
-        result = copy.deepcopy(self._conf['m2ee'].get('jetty', {}))
-        already_specified = 'use_blocking_connector' in result
-        if self.get_runtime_version() >= 5 and not already_specified:
-            result['use_blocking_connector'] = (
-                self.get_runtime_blocking_connector())
-
-        return result
+        jetty_opts = copy.deepcopy(self._conf['m2ee'].get('jetty'))
+        if jetty_opts is None:
+            jetty_opts = {}
+        if self.get_runtime_version() >= 5:
+            jetty_opts['use_blocking_connector'] = (
+                jetty_opts.get('use_blocking_connector',
+                               self.get_runtime_blocking_connector()))
+        return jetty_opts
 
     def get_munin_options(self):
         return self._conf['m2ee'].get('munin', None)