e59ebfec438a407af28c045d57bd9139273742e9
Jouke Waleson <jouke.waleson@mendix.com>
2016-10-31 12:17:47 +0100

    clarify mx7 compatility
    the java arguments / classpath code is rather convoluted
    
    although the if statements in this commit should not be reached, it
    improves the code reading / debugging process

--- src/m2ee/config.py
+++ src/m2ee/config.py
@@ -107,6 +107,8 @@ class M2EEConfig:
             logger.debug("Building classpath to run hybrid appcontainer from "
                          "source.")
             classpath = self._setup_classpath_from_source()
+        elif self.runtime_version >= 7:
+            logger.debug('Mendix 7 uses runtime-launcher.jar, no classpath')
         elif self.use_hybrid_appcontainer() and self.runtime_version < 5:
             logger.debug("Hybrid appcontainer from jars does not need a "
                          "classpath.")
@@ -715,7 +717,7 @@ class M2EEConfig:
             if self.use_hybrid_appcontainer():
                 return "com.mendix.m2ee.AppContainer"
             return "com.mendix.m2ee.server.HttpAdminAppContainer"
-        if self.runtime_version >= 5:
+        if self.runtime_version >= 5 and self.runtime_version < 7:
             return "org.apache.felix.main.Main"
 
         raise Exception("Trying to determine appcontainer main class for "
@@ -758,7 +760,7 @@ class M2EEConfig:
                 os.path.join(self._runtime_path, 'runtime', '*'),
                 os.path.join(self._runtime_path, 'runtime', 'lib', '*'),
             ])
-        elif self.runtime_version >= 5:
+        elif self.runtime_version >= 5 and self.runtime_version < 7:
             classpath.extend([
                 os.path.join(self._runtime_path, 'runtime', 'felix', 'bin',
                              'felix.jar'),
