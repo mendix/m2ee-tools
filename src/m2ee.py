mendix/m2ee-tools

commit e5fee3a4c7f4616a0ca2af5267c2dda982740e17

Mendix 7 compatibility
...and it's not using OSGI any more.

* New start point is runtimelauncher.jar, without classpath.
* The MX_INSTALL_PATH contains the same value as RuntimePath, which is
  still injected into the configuration. It's needed before the actual
  Mendix Runtime code is started, so we have to provide it in the env
  also now.
* Update >=5 code to check for 5 or 6 if applicable, even if the code
  path is not reached for 7.

Thanks Jouke for first version of the patch.

src/m2ee/config.py
@@ -67,8 +67,8 @@ class M2EEConfig:
                                 "download_runtime command." %
                                 str(self.runtime_version))
                     self._all_systems_are_go = False
-
-        self._setup_classpath()
+        if self.runtime_version < 7:
+            self._setup_classpath()
 
         if self._runtime_path and 'RuntimePath' not in self._conf['mxruntime']:
             runtimePath = os.path.join(self._runtime_path, 'runtime')
@@ -482,6 +482,9 @@ class M2EEConfig:
             env['M2EE_MONITORING_PASS'] = str(
                 self._conf['m2ee']['monitoring_pass'])
 
+        if self.runtime_version >= 7:
+            env['MX_INSTALL_PATH'] = self._runtime_path
+
         return env
 
     def get_java_cmd(self):
@@ -497,7 +500,13 @@ class M2EEConfig:
             else:
                 logger.warn("javaopts option in m2ee section in configuration "
                             "is not a list")
-        if self._classpath:
+        if self.runtime_version >= 7:
+            cmd.extend([
+                '-jar',
+                os.path.join(self._runtime_path, 'runtime/launcher/runtimelauncher.jar'),
+                self.get_app_base(),
+            ])
+        elif self._classpath:
             cmd.extend(['-cp', self._classpath])
 
             if self.runtime_version >= 5:
@@ -679,7 +688,7 @@ class M2EEConfig:
             if self.use_hybrid_appcontainer():
                 return "com.mendix.m2ee.AppContainer"
             return "com.mendix.m2ee.server.HttpAdminAppContainer"
-        if self.runtime_version >= 5:
+        if self.runtime_version // 5 or self.runtime_version // 6:
             return "org.apache.felix.main.Main"
 
         raise Exception("Trying to determine appcontainer main class for "
@@ -722,13 +731,16 @@ class M2EEConfig:
                 os.path.join(self._runtime_path, 'runtime', '*'),
                 os.path.join(self._runtime_path, 'runtime', 'lib', '*'),
             ])
-        elif self.runtime_version >= 5:
+        elif self.runtime_version // 5 or self.runtime_version // 6:
             classpath.extend([
                 os.path.join(self._runtime_path, 'runtime', 'felix', 'bin',
                              'felix.jar'),
                 os.path.join(self._runtime_path, 'runtime', 'lib',
                              'com.mendix.xml-apis-1.4.1.jar')
             ])
+        else:
+            raise Exception("Trying to determine runtime classpath for runtime version %s. "
+                            "Please report this as a bug." % self.runtime_version)
 
         return classpath
 

src/m2ee/core.py
@@ -72,7 +72,7 @@ class M2EE():
 
         version = self.config.get_runtime_version()
 
-        if version >= 5:
+        if version // 5 or version // 6:
             self.config.write_felix_config()
 
         if self.config.get_symlink_mxclientsystem():

commit c9595a1aec4700453d56f96fa562fa0c64f64ee2

cli,config: Fix explode on reload without model
Right... When there's no unpacked application model, this just explodes:

File "/home/knorrie/s/m2ee-tools/src/m2ee/config.py", line 63, in __init__
  if self.runtime_version < 7:
TypeError: '<' not supported between instances of 'NoneType' and 'int'

This is broken since commit e5fee3a4c7. Fix it by exiting the config
reload sequence when we already know it has no use to try to continue.

Also, if we attempt start without having an application model in place,
then don't complain about "the Mendix Runtime version which has to be
used" because we're not even that far yet.

src/m2ee.py
@@ -105,6 +105,12 @@ class CLI(cmd.Cmd, object):
         way. See the client_errno for possible error codes.
         """
 
+        if not self.m2ee.config.all_systems_are_go():
+            raise m2ee.exceptions.M2EEException(
+                "The application cannot be started because no application "
+                "model is present, or because of other previous errors."
+            )
+
         if not self.m2ee.config.get_runtime_path():
             raise m2ee.exceptions.M2EEException(
                 "It appears that the Mendix Runtime version which has to be "

src/m2ee/config.py
@@ -45,21 +45,22 @@ class M2EEConfig:
             ))
 
         self.runtime_version = self._lookup_runtime_version()
-
-        self._runtime_path = None
         if self.runtime_version is None:
             logger.info("Unable to look up mendix runtime files "
-                        "because product version is yet unknown.")
+                        "because product version is yet unknown. "
+                        "Try unpacking a deployment archive first.")
             self._all_systems_are_go = False
-        else:
-            self._runtime_path = self.lookup_in_mxjar_repo(
-                str(self.runtime_version))
-            if self._runtime_path is None:
-                logger.warn("Mendix Runtime not found for version %s. "
-                            "You can try downloading it using the "
-                            "download_runtime command." %
-                            str(self.runtime_version))
-                self._all_systems_are_go = False
+            return
+
+        self._runtime_path = self.lookup_in_mxjar_repo(str(self.runtime_version))
+        if self._runtime_path is None:
+            logger.warn("Mendix Runtime not found for version %s. "
+                        "You can try downloading it using the "
+                        "download_runtime command." %
+                        str(self.runtime_version))
+            self._all_systems_are_go = False
+            return
+
         if self.runtime_version < 7:
             self._setup_classpath()
 